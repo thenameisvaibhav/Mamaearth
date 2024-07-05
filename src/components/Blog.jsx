6a5b3d6e073d810cc73f899decce7b1a09f136c0
Vaibhav Agarwal <vjindal742@gmail.com>
2024-07-05 17:55:54 +0530

    minor2

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -4,15 +4,12 @@ import { FaArrowDown } from "react-icons/fa6";
 
 const Blog = () => {
     return (
-        <motion.div 
-            initial={{ y: '300%', opacity: 0 }}
-            whileInView={{ y: '0%', opacity: 1 }}
-            viewport={{ once: true }}
-            transition={{ ease: [0.16, 1, 0.3, 1], duration: .5 }} className='w-full mt-5 px-5 font-["Helvetica"] text-center'>
+        <div 
+            className='w-full mt-5 px-5 font-["Helvetica"] text-center'>
             <h2 className='text-[1.8rem] md:text-[3.5rem] font-semibold'>Want to become younger!</h2>
             <FaArrowDown className='arrow mt-2 text-4xl md:text-[3.5rem] inline-block relative md:left-56 hover:rotate-180 hover:transition-all' />
             <h4 className='text-3xl md:text-[3.5rem] mt-2'>Then checkout our <a href='#' className='uppercase font-bold border-[1px] underline underline-offset-4 hover:underline-offset-8 hover:transition-all'>Blog</a></h4>
-        </motion.div>
+        </div>
     )
 }
 
--- src/components/Tree.jsx
+++ src/components/Tree.jsx
@@ -17,14 +17,8 @@ const Tree = () => {
               animate={{ y: '0%' }}
               transition={{ ease: [0.87, 0, 0.13, 1], delay: index * .05 }}
               className='inline-block text-4xl md:text-7xl lg:text-8xl px-5 md:px-10 lg:px-14 tracking-wide font-semibold font-["Helvetica"] mb-2 md:mb-4 lg:mb-6 text-center lg:mt-14 lg:ml-20' key={index}>{item}</motion.span>)}
-          <motion.p initial={{ y: '300%', opacity: 0 }}
-            whileInView={{ y: '0%', opacity: 1 }}
-            viewport={{ once: true }}
-            transition={{ ease: [0.16, 1, 0.3, 1], duration: .5 }} className='text-center text-sm lg:mt-20'>Trees have got life already...</motion.p>
-          <motion.p initial={{ y: '300%', opacity: 0 }}
-            whileInView={{ y: '0%', opacity: 1 }}
-            viewport={{ once: true }}
-            transition={{ ease: [0.16, 1, 0.3, 1], duration: .5 }} className='text-center text-sm mb-4 md:mb-8 lg:mb-12'>Everytime you buy from us, we plant more trees!</motion.p>
+          <p className='text-center text-sm lg:mt-20'>Trees have got life already...</p>
+          <p className='text-center text-sm mb-4 md:mb-8 lg:mb-12'>Everytime you buy from us, we plant more trees!</p>
         </div>
       </div>
     </div>
